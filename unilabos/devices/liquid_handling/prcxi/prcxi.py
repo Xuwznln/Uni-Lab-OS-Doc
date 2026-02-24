0dc273f3664e96ebcf37267e9a82b6eaaedacde8
q434343 <554662886@qq.com>
2026-02-24 19:37:11 +0800

    修改上传方式，添加tip_rack的连线

--- unilabos/devices/liquid_handling/prcxi/prcxi.py
+++ unilabos/devices/liquid_handling/prcxi/prcxi.py
@@ -721,7 +721,9 @@ class PRCXI9300Handler(LiquidHandlerAbstract):
         delays: Optional[List[int]] = None,
         none_keys: List[str] = [],
     ) -> TransferLiquidReturn:
-        return await super().transfer_liquid(
+        if self.step_mode:
+            await self.create_protocol(f"transfer_liquid{time.time()}")
+        res =  await super().transfer_liquid(
             sources,
             targets,
             tip_racks,
@@ -744,6 +746,9 @@ class PRCXI9300Handler(LiquidHandlerAbstract):
             delays=delays,
             none_keys=none_keys,
         )
+        if self.step_mode:
+            await self.run_protocol()
+        return res
 
     async def custom_delay(self, seconds=0, msg=None):
         return await super().custom_delay(seconds, msg)
