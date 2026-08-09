53033eef01fe93dd805240b0cd7a2bc433668b1e
Xuwznln <18435084+Xuwznln@users.noreply.github.com>
2026-08-10 02:13:25 +0800

    test(edge): gate integrated scheduler bridge

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 from setuptools import setup, find_packages
 
-package_name = 'unilabos'
+package_name = "unilabos"
 
 setup(
     name=package_name,
-    version='0.11.3',
+    version="0.11.3",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
     author="The unilabos developers",
-    maintainer='Junhan Chang, Xuwznln',
-    maintainer_email='Junhan Chang <changjh@pku.edu.cn>, Xuwznln <18435084+Xuwznln@users.noreply.github.com>',
-    description='',
-    license='GPL v3',
-    tests_require=['pytest'],
+    maintainer="Junhan Chang, Xuwznln",
+    maintainer_email="Junhan Chang <changjh@pku.edu.cn>, Xuwznln <18435084+Xuwznln@users.noreply.github.com>",
+    description="",
+    license="GPL v3",
+    tests_require=["pytest", "hypothesis"],
     entry_points={
-        'console_scripts': [
-            "unilab = unilabos.app.main:main"
-        ],
+        "console_scripts": ["unilab = unilabos.app.main:main"],
     },
 )
