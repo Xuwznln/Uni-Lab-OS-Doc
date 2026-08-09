3af6af943377e21622ed3ffaebf54991b4b575ac
Xuwznln <18435084+Xuwznln@users.noreply.github.com>
2026-08-10 03:57:26 +0800

    release: bump Jazzy packages to 0.12.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ package_name = 'unilabos'
 setup(
     name=package_name,
     python_requires='>=3.12,<3.13',
-    version='0.11.3',
+    version='0.12.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['setuptools'],
